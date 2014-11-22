9be6154faf4d3310237abe8426d2fcd3bb610f34
Kelly Myers <kelbel0323@yahoo.com>
2014-11-22 12:09:46 -0700

    extracted js from html file

--- mondrian.js
+++ mondrian.js
@@ -2,3 +2,19 @@
  * Created by kelly on 11/19/14.
  */
 
+$(document).ready(function () {
+
+    var colors = $('.colors');
+    var line = $('.line');
+    var coloredSquare = "";
+
+    colors.on('click', function () {
+        coloredSquare = $(this).css('background-color');
+    });
+
+    line.on('click', function () {
+        $(this).css('background-color', coloredSquare);
+    });
+
+});
+
